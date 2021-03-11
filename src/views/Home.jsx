23287711d1f09d1c5ad21ef3b3c35f5a2c8987e0
Jesus Hernandez <hdmjesus@gmail.com>
2021-03-11 12:07:02 -0400

    componets create

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserList from "../componets/Home/UserList";
 import UserCard from "../componets/Home/UserCard";
 import Header from "../componets/Header/Header";
 
 const Home = (props) => {
+  const [state, setstate] = useState(initialState);
+
+  useEffect(() => {}, []);
   return (
     <>
       <Header />
